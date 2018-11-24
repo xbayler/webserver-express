c9512ad5afd64f3da4b2c78e404171d2bbad6772
Sergio <sergio.ascanio.94@gmail.com>
2018-11-24 14:50:46 +0100

    Primer commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+app.get('/', function(req, res) {
+    res.render('home.hbs', {
+        nombre: 'sergio aSCANIO mÁrquez'
+    });
+});
+
+app.get('/about', function(req, res) {
+    res.render('about.hbs');
+});
+
+app.listen(port, () => console.log(`Escuchando peticiones en el puerto ${ port }.`));
